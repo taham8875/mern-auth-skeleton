d651cead22a047e1b297d2733f3b3b2e6fab75b2
taham8875 <taha.m8875@gmail.com>
2023-09-08 23:48:57 +0300

    refactor: Improve server setup and move database connection to separate file
    - Use type import for `Application`, `Request`, and `Response` from "express" for better TypeScript support.
    - Reorganize middleware and route setup for better readability.
    - Move the database connection logic to a separate file (`config/db.ts`) for modularity.~

--- backend/server.ts
+++ backend/server.ts
@@ -1,44 +1,40 @@
-import express, { Application, Request, Response } from "express";
-import mongoose from "mongoose";
+import express from "express";
+import type { Application, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
-import { authUser } from "./controller/user.controller.ts";
 import userRouter from "./routes/user.route.ts";
 import { notFoundHandler, errorHandler } from "./middleware/error.middleware";
+import connectDB from "./config/db.ts";
 
 const port: String | Number = process.env.PORT || 5000;
 
 const app: Application = express();
 
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+// Routes
 app.use("/api/users", userRouter);
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
+
+// Database connection
+connectDB();
+
 // If the request reaches this point, it means that the request was not handled by any of the routes.
 app.use(notFoundHandler);
-
 app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-// Connect to MongoDB
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI as string);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (error: any) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
-};
 
-connectDB();
