4297381f43d8c8e9b936cb58d4e92820803d3eb5
taham8875 <taha.m8875@gmail.com>
2023-09-08 23:45:24 +0300

    refactor: Use type import and clarify variable names in generateToken utility
    - Import type { Response } from "express" for better TypeScript support.
    - Renamed the "id" parameter to "userId" for clarity.
    - Remove redundant commit in the "secure" property.

--- backend/utils/generateToken.util.ts
+++ backend/utils/generateToken.util.ts
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken";
-import { Response } from "express";
+import type { Response } from "express";
 
-const generateToken = (res: Response, id: string) => {
-  const token = jwt.sign({ id }, process.env.JWT_SECRET as string, {
+const generateToken = (res: Response, userId: string) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET as string, {
     expiresIn: "30d",
   });
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // Set to true in production
+    secure: process.env.NODE_ENV !== "development",
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
