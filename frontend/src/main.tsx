3ad5e966a72c395016932ced41104ba53357cb8c
taham8875 <taha.m8875@gmail.com>
2023-09-09 02:54:23 +0300

    feat: Integrate Redux store and routes for login and registration
    - Integrated the Redux store using react-redux to manage application state.
    - Added routes for login and registration using react-router-dom.
    - Connected the Redux store to the application using the Provider component.
    ~

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,17 +5,26 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import store from "./store.ts";
+import { Provider } from "react-redux";
 import App from "./App.tsx";
 import HomeScreen from "./screens/HomeScreen.tsx";
+import LoginScreen from "./screens/LoginScreen.tsx";
+import RegisterScreen from "./screens/RegisterScreen.tsx";
+
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={routes} />
+  <Provider store={store}>
+    <RouterProvider router={routes} />
+  </Provider>
 );
