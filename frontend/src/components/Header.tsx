48b1a5dbb9a24187659c80f12ec4dfadba907ad0
taham8875 <taha.m8875@gmail.com>
2023-09-09 02:53:11 +0300

    feat: Enhance header component with user authentication and logout functionality
    - Added dynamic rendering of navigation links based on user authentication status.
    - Implemented user logout functionality with Redux and react-toastify for notifications.
    - Improved UI by displaying the user email when authenticated.
    - Toast notifications are used for successful and failed logout attempts.1~

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,50 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout as logoutReducer } from "../slices/authSlice";
+import { toast } from "react-toastify";
 
 function Header() {
+  const {userInformation} = useSelector((state: any) => state.auth);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [logout] = useLogoutMutation();
+
+  const handleLogout = async () => {
+    try {
+      await logout({}).unwrap();
+      dispatch(logoutReducer());
+      navigate("/");
+      toast.success ("Logged out successfully", {
+        position: "top-right",
+        autoClose: 2500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    } catch (error) {
+      toast.error (error?.data?.message || error?.error, {
+        position: "bottom-center",
+        autoClose: 2500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
+
+
   return (
     <header>
       <Navbar bg="dark" data-bs-theme="dark" expand="sm" collapseOnSelect>
@@ -13,18 +55,37 @@ function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  {" "}
-                  <FaSignInAlt /> Sign in{" "}
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/register">
-                <Nav.Link>
-                  {" "}
-                  <FaSignOutAlt /> Log out{" "}
-                </Nav.Link>
-              </LinkContainer>
+              {userInformation ? (
+                <>
+                  <LinkContainer to="/profile">
+                    <Nav.Link>
+                      {" "}
+                      <FaSignInAlt /> {userInformation.email}{" "}
+                    </Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/" onClick={handleLogout}>
+                    <Nav.Link>
+                      {" "}
+                      <FaSignInAlt /> Logout{" "}
+                    </Nav.Link>
+                  </LinkContainer>
+                </>
+              ) : (
+                <>
+                  <LinkContainer to="/login">
+                    <Nav.Link>
+                      {" "}
+                      <FaSignInAlt /> Sign in{" "}
+                    </Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/register">
+                    <Nav.Link>
+                      {" "}
+                      <FaSignInAlt /> Sign Up{" "}
+                    </Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
