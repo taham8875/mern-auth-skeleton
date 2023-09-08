52942945d08e1098a06a885fb23d513bc9b7a571
taham8875 <taha.m8875@gmail.com>
2023-09-09 02:50:12 +0300

    style: Add a toast notification container and adjust styles
    - Introduced a toast notification container using react-toastify.
    - Adjusted styles for better UI consistency.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
       <Header />
+      <ToastContainer
+        position="bottom-center"
+        autoClose={2500}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Outlet />
     </>
   );
