838bc98b08fc37351cba2c9b1c2d2b73b4e02e49
taham8875 <taha.m8875@gmail.com>
2023-09-09 03:03:58 +0300

    feat: Implement Redux slice for authentication
    - Created a Redux slice named authSlice for managing user authentication state.
    - Defined initial state including userInformation retrieved from localStorage.
    - Added setCredentials reducer to update user information and store it in localStorage.
    - Implemented logout reducer to clear user information and remove it from localStorage.

---
+++ frontend/src/slices/authSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userInformation: localStorage.getItem("userInformation")
+    ? JSON.parse(localStorage.getItem("userInformation") as string)
+    : null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInformation = action.payload;
+      localStorage.setItem("userInformation", JSON.stringify(action.payload));
+    },
+    logout: (state) => {
+      state.userInformation = null;
+      localStorage.removeItem("userInformation");
+    },
+  },
+});
+
+export const { setCredentials, logout } = authSlice.actions;
+
+export default authSlice.reducer;
